afa2b52c24b8baa78ecc8a3d94152b04d06b579c
hyojoonm <ssjj008z@naver.com>
2022-09-02 00:14:59 +0900

    commit

---
+++ src/bj_1181.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class bj_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        String[] str = new String[num];
+
+        for(int i = 0;i<num;i++){
+            str[i] = br.readLine();
+        }
+
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                else{
+                    return o1.length() -o2.length();
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(str[0]).append("\n");
+
+        for(int i = 1 ; i<num;i++){
+            if(!str[i].equals(str[i-1])){
+                sb.append(str[i]).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
