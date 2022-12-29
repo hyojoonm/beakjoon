ddb50dfdc85b176df81b0ac1afcd88351e197d27
hyojoonm <ssjj008z@naver.com>
2022-12-29 13:53:28 +0900

    버블 정렬

--- src/bj_1377.java
+++ src/bj_1377.java
@@ -13,12 +13,17 @@ public class bj_1377 {
         mData[] arr = new mData[n];
 
         for (int i = 0;i<n;i++){
+            // 숫자 기준으로 오름차순으로 정렬해서 (정렬 전 index) 를 정렬
+            /* ex) data			1	2	3	5	10
+                  정렬 전 idnex	1	3	4	2	0
+              */
             arr[i] = new mData(Integer.parseInt(br.readLine()),i);
         }
         Arrays.sort(arr);
         int max = 0;
         for (int i =0;i<n;i++){
-            if(max < arr[i].index-i){
+
+            if(max < arr[i].index-i){   // 정렬 전 index - 정렬 후 index 계산의 최댓값 저장
                 max = arr[i].index -i;
             }
         }
@@ -33,12 +38,13 @@ class mData implements Comparable<mData>{
     int index;
 
     public mData(int value,int index){
-        super();
+
         this.value =value;
         this.index = index;
     }
+    // value 기준 오름차순 정렬 함수 comparable 구현
     @Override
-    public int compareTo(mData o){
+    public int compareTo(mData o){  // value 기준 오름차순 정렬
         return this.value - o.value;
     }
 }
