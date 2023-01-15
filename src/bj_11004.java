0610f7c71db1a8f10145d3b33f94c19b86613192
hyojoonm <ssjj008z@naver.com>
2023-01-15 23:53:04 +0900

    퀵 정렬

--- src/bj_11004.java
+++ src/bj_11004.java
@@ -21,8 +21,57 @@ public class bj_11004 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        quickSort(arr,0,n-1,k-1);
 
         System.out.println(arr[k-1]);
+    }
+
+    private static void quickSort(int[] arr, int s, int e, int k) {
+        if(s<e){
+            int pivot = partition(arr,s,e);
+            if (pivot == k)
+                return;
+            else if (k < pivot)
+            {
+                quickSort(arr,s,pivot-1,k);
+            }
+            else
+                quickSort(arr,pivot+1,e,k);
+        }
+    }
+
+    private static int partition(int[] arr, int s, int e) {
+        if(s+1 == e){
+            if(arr[s] > arr[e])
+                swap(arr,s,e);
+            return e;
+        }
+        int m = (s+e)/2;
+        swap(arr,s,m);
+        int pivot = arr[s];
+
+        int i = s+1 , j =e;
+
+        while (i <=j){
+            while (pivot < arr[j] && j >0){
+                j--;
+            }
+            while (pivot > arr[i] && i< arr.length-1){
+                i++;
+            }
+            if (i <= j){
+                swap(arr,i++,j--);
+            }
+        }
+
+        arr[s] = arr[j];
+        arr[j] = pivot;
+        return j;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
