b7b4bbd7adb1668225a407de4cf8cc9e5a1f976c
hyojoonm <ssjj008z@naver.com>
2023-02-06 16:43:28 +0900

    dfs

---
+++ src/bj_13023.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_13023 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[n];
+        for (int i =0; i < m;i++){
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int i1 = Integer.parseInt(st1.nextToken());
+            num[st1] =
+        }
+    }
+}
