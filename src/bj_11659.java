5b8131d1c059d6f9510387d19172081e6f90bb8c
hyojoonm <ssjj008z@naver.com>
2023-02-06 23:42:20 +0900

    구간 합

--- src/bj_11659.java
+++ src/bj_11659.java
@@ -7,41 +7,26 @@ public class bj_11659 {
     public static void main(String[] args)  throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
 
-
-       /*  리펙토링 전
-       int[] arr = new int[a+1];
-       StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-
-        for (int i = 1 ; i<=a ; i++){
-            arr[i] = Integer.parseInt(st1.nextToken());
-        }
-        // 합배열 생성
         int[] sum = new int[a+1];
-        sum[1] = arr[1];
-        for (int i = 2; i <=a; i++){
-            sum[i] = sum[i-1] + arr[i];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i<=a ; i++){
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
         }
-        */
-        // 리펙토링 후 기존 배열은 문제에서 필요가 없으니 그냥 합배열 생성
-        long[] sum = new long[a+1];
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        for (int i =1;i<=a;i++){
-            sum[i] = sum[i-1] + Integer.parseInt(st1.nextToken());
+
+        for (int i = 0; i < b; i ++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            System.out.println(sum[m] - sum[n-1]);
         }
-        for (int i = 0; i<b;i++){
-            StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-            int c = Integer.parseInt(st2.nextToken());
-            int d = Integer.parseInt(st2.nextToken());
-            // ex) 1, 3 일 경우 sum[3] - sum[1-0] = 12 - 0 = 12
-            // 합 배열 생성하면 연산과정 생략 가능
-            System.out.println(sum[d] - sum[c-1]);
-        }
+
+
 
     }
 }
--- src/bj_11660.java
+++ src/bj_11660.java
@@ -5,43 +5,31 @@ public class bj_11660 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(st.nextToken());
-        int num = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[n+1][n+1];
+        int m = Integer.parseInt(st.nextToken());
+        int[][] num = new int[n+1][n+1];
         int[][] sum = new int[n+1][n+1];
-        for (int i =1; i<=n;i++){
-
+        for (int i = 1 ; i <= n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j=1;j<=n;j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <=n ; j++){
+                num[i][j] =  Integer.parseInt(st.nextToken());
             }
         }
 
-        // 합 배열 생성
-        for (int i =1; i<=n;i++){
-            for (int j=1;j<=n;j++){
-                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + arr[i][j];
+        for (int i = 1; i<=n;i++){
+            for (int j = 1 ; j<=n; j++){
+                sum[i][j] = sum[i-1][j] +sum[i][j-1] - sum[i-1][j-1]+num[i][j];
             }
         }
 
 
-        for (int i =0;i<num;i++){
+        for (int i = 0 ; i < m;i++){
             st = new StringTokenizer(br.readLine());
-             int x1 = Integer.parseInt(st.nextToken());
-             int y1 = Integer.parseInt(st.nextToken());
-             int x2 = Integer.parseInt(st.nextToken());
-             int y2 = Integer.parseInt(st.nextToken());
-
-            int result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
-            sb.append(result).append('\n');
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            System.out.println(sum[x2][y2] - sum[x2][y1-1] - sum[x1-1][y2] + sum[x1-1][y1-1]);
         }
-        bw.write(sb+"");
-        bw.flush();
-        bw.close();
-
     }
 }
